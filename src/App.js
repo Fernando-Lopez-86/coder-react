d90b6af6dccee06dad5289724985e9f56e994716
Fernando <ferdinandolopez@hotmail.com>
2021-08-09 22:13:35 -0300

    desafio4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
+import Button from "./components/Button";
 import Route from 'react-router-dom/Route';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -16,25 +17,41 @@ import Agregar from './Pages/agregar';
 // Semantic UI
 import { Button } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    };
+  }
 
-        <BrowserRouter>
-          <div>
-            <Navbar />
+  incrementaContador = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  decrementaContador = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  };
+
+  render() {
+    let { count } = this.state;
+    return (
+      <div className="app">
+        <div>
+          <div class="contador">
+            <h3>Contador:</h3>
+            <h1>{count}</h1>
           </div>
-        </BrowserRouter>
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          LA TIENDA DE MARIANA
-        </a>
-      </header>
-    </div>
-  );
+          <div class="boton">
+            <Button title={"-"} action={this.decrementaContador} />
+            <Button title={"+"} action={this.incrementaContador} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
-
